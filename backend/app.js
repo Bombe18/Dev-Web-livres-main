72625b8e82a0b7ad14e1ddd6bba6d7a141aa3ce3
Colin Cyril <cyrilco18@gmail.com>
2026-03-25 11:38:35 +0100

    Added mongoose, connexion to mangoose, added express, node, nodemon.

---
+++ backend/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+//password : 7KjAwO5n5bERzC7Z
+
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://Cyril:7KjAwO5n5bERzC7Z@cluster0.e9zjdut.mongodb.net/?appName=Cluster0";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
+  
+const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json ({message: "objets crée"})
+});
+
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
